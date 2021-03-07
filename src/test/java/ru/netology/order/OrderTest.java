361ce3d39b9e537f29706213e84220043b42eaf2
Lyubov Zabavina <zabavinal@yandex.ru>
2021-03-07 14:13:25 +0300

    test added (selenide)

--- src/test/java/ru/netology/order/OrderTest.java
+++ src/test/java/ru/netology/order/OrderTest.java
@@ -1,5 +1,6 @@
 package ru.netology.order;
 
+import static com.codeborne.selenide.Condition.exactText;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -7,6 +8,11 @@ public class OrderTest {
 
     @Test
     void shouldOpen() {
-        open("http://localhost:9999");
+        open("http://localhost:7777");
+        $("[data-test-id = name] input").setValue("Иванов Петр");
+        $("[data-test-id = phone] input").setValue("+79017007729");
+        $("[data-test-id = agreement]").click();
+        $("[type = button]").click();
+        $("[data-test-id = order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
